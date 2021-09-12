b60cece069516b03abe4e68552f5352e7e677f28
wen kai <clwk94.work@gmail.com>
2021-09-12 20:45:40 +0800

    feat: add sample navbar to landing page

--- src/app/main/main/main.component.ts
+++ src/app/main/main/main.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  menuActive: boolean =  false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggle(){
+    this.menuActive = !this.menuActive;
   }
 
 }
